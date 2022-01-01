0dc3a2fa54e52c96d0d7ae68416b3c2c5002dc6e
DSiedlarz90 <dsiedlarz90@gmail.com>
2022-01-01 09:02:52 -0500

    added vouter routes and table manipulation to files

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const db = require('./db/connection');
 const express = require('express');
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,9 +9,10 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
